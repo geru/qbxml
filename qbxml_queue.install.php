2ae262b15882563547ef71e2aceb68c51e9d9628
geru <hkern0@gmail.com>
2016-08-20 21:54:07 -0700

    initialize qbxml_q repo

---
+++ qbxml_queue.install.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugh
+ * Date: 5/5/2015
+ * Time: 2:16 PM
+ */
---
+++ theme/node--qb-command--qbxml.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugh
+ * Date: 7/14/2015
+ * Time: 2:39 PM
+ */
+?>
+<?php    print render($content);
+?>
